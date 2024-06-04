f574202246b9e8c5afb3194229fe29c45f313936
izdener <izdener@proton.me>
2024-06-04 07:48:46 +0200

    Greetd test

---
+++ config/scripts/example copy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo 'Creating the greeter user'
+
+useradd -M -G video greeter
+chmod -R go+r /etc/greetd/
+
+echo 'Enable greetd'
+systemctl enable --now greetd
